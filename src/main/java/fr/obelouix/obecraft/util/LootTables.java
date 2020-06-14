39be2c5eba0ea92c370ee7515aa9a02e7319ed73
Louis <obelouix25@gmail.com>
2020-06-14 19:41:51 +0200

    added data generator

--- src/main/java/fr/obelouix/obecraft/util/LootTables.java
+++ src/main/java/fr/obelouix/obecraft/util/LootTables.java
@@ -1,4 +1,16 @@
 package fr.obelouix.obecraft.util;
 
-public class LootTables {
+import fr.obelouix.obecraft.blocks.Blocks;
+import net.minecraft.data.DataGenerator;
+
+public class LootTables extends BaseLootTableProvider {
+
+    public LootTables(DataGenerator dataGeneratorIn) {
+        super(dataGeneratorIn);
+    }
+
+    @Override
+    protected void addTables() {
+        lootTables.put(Blocks.URANIUM_ORE.get().getBlock(), createStandardTable("uranium_ore", Blocks.URANIUM_ORE.get().getBlock()));
+    }
 }
--- src/main/java/fr/obelouix/obecraft/util/Recipes.java
+++ src/main/java/fr/obelouix/obecraft/util/Recipes.java
@@ -1,9 +1,29 @@
 package fr.obelouix.obecraft.util;
 
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.IDataProvider;
+import fr.obelouix.obecraft.blocks.Blocks;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.data.*;
+import net.minecraftforge.common.Tags;
 
-public class Recipes implements IDataProvider {
-    public Recipes(DataGenerator generator) {
+import java.util.function.Consumer;
+
+public class Recipes extends RecipeProvider {
+
+    public Recipes(DataGenerator generatorIn) {
+        super(generatorIn);
+    }
+
+    @Override
+    protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        //Will be removed later , only used it for exampler
+        ShapedRecipeBuilder.shapedRecipe(Blocks.URANIUM_ORE.get())
+                .patternLine("xxx")
+                .patternLine("x#x")
+                .patternLine("xxx")
+                .key('x', net.minecraft.block.Blocks.COBBLESTONE)
+                .key('#', Tags.Items.DYES_RED)
+                .setGroup("mytutorial")
+                .addCriterion("cobblestone", InventoryChangeTrigger.Instance.forItems(net.minecraft.block.Blocks.COBBLESTONE))
+                .build(consumer);
     }
 }
