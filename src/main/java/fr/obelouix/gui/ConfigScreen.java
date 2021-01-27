88ff65265e8de993f24addb8109b849ad2eecd4d
Louis <3874662+obelouix@users.noreply.github.com>
2021-01-27 12:29:44 +0100

    added drawing of options

--- src/main/java/fr/obelouix/gui/ConfigScreen.java
+++ src/main/java/fr/obelouix/gui/ConfigScreen.java
@@ -48,13 +48,12 @@ public class ConfigScreen extends Screen {
     protected void init() {
         this.optionsRowList = new OptionsRowList(this.minecraft,
                 this.width, this.height, OPTIONS_LIST_TOP_HEIGHT,
-                OPTIONS_LIST_BOTTOM_OFFSET, OPTIONS_LIST_ITEM_HEIGHT);
+                this.height - OPTIONS_LIST_BOTTOM_OFFSET, OPTIONS_LIST_ITEM_HEIGHT);
 
         this.optionsRowList.addOption(new BooleanOption("obecraft.configGUI.flatbedrock.title",
         unused -> Config.isFlatBedrock(), (unused, newvalue) -> Config.setFlatBedrock(newvalue)));
-
-
         this.children.add(optionsRowList);
+
         this.addButton(new Button((this.width - BUTTON_WIDTH ) / 2,
                 this.height - DONE_BUTTON_TOP_OFFSET,
                 BUTTON_WIDTH, BUTTON_HEIGHT,
@@ -65,6 +64,7 @@ public class ConfigScreen extends Screen {
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
         this.renderBackground(matrixStack);
+        this.optionsRowList.render(matrixStack, mouseX, mouseY, partialTicks);
         drawCenteredString(matrixStack,this.font, this.title, this.width/2, TITLE_HEIGHT, 0xFFFFFF);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
     }
@@ -73,6 +73,8 @@ public class ConfigScreen extends Screen {
     public void closeScreen() {
        // ModSettings.save();
         // Display the parent screen
+        Config.saveCommon();
+        Config.saveClient();
         this.minecraft.displayGuiScreen(parentScreen);
     }
 }
